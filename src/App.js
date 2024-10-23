064ea02354d6cb6978670ef8cbafa6d0ac00f406
Ayushi Gupta <133566741+AyushiGupta160604@users.noreply.github.com>
2024-10-24 03:26:06 +0530

    added frontend files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './styles.css';  // Importing the stylesheet
+import CreateRule from './components/CreateRule';
+import EvaluateRule from './components/EvaluateRule';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Rule Engine with AST</h1>
+      <CreateRule />
+      <EvaluateRule />
+    </div>
+  );
+}
+
+export default App;
